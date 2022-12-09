b4552016e99d7a70c76c5572abdb9c8800784a69
Muhammad Azeez <muhammad-azeez@outlook.com>
2022-12-09 17:59:02 +0300

    refactor

--- Interop.cs
+++ Interop.cs
@@ -12,9 +12,6 @@ public class Interop
     public static extern int CountVowelsNative();
 
     [MethodImpl(MethodImplOptions.InternalCall)]
-    public static unsafe extern byte* GetInput();
-
-    [MethodImpl(MethodImplOptions.InternalCall)]
     public static extern long extism_input_length();
 
     [MethodImpl(MethodImplOptions.InternalCall)]
@@ -34,39 +31,7 @@ public class Interop
 
     public unsafe static int count_vowels()
     {
-        var length = extism_input_length();
-        if (length == 0)
-        {
-            return 0;
-            // return native_power(0);
-            //var b = do_something(0);
-        }
-
-        //fixed (byte* bytesPtr = bytes)
-        //{
-        //    load_input(bytesPtr, length);
-        //}
-
-        //   return 92;
-
-        var buffer = new byte[length];
-
-        //extism_input_load_u8(0);
-
-        for (int i = 0; i < length; i++)
-        {
-            //if (length - i >= 8)
-            //{
-            //    var x = extism_input_load_u64(i);
-            //    var bytes = bitconverter.getbytes(x);
-            //    array.copy(bytes, 0, buffer, i, bytes.length);
-            //    i += 7;
-            //}
-            //else
-            //{
-            buffer[i] = extism_input_load_u8(i);
-            // }
-        }
+        byte[] buffer = GetInput();
 
         var text = Encoding.UTF8.GetString(buffer);
 
@@ -87,5 +52,33 @@ public class Interop
         }
 
         return count;
+    }
+
+    private static unsafe byte[] GetInput()
+    {
+        var length = extism_input_length();
+        if (length == 0)
+        {
+            return Array.Empty<byte>();
+        }
+
+        var buffer = new byte[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            if (length - i >= 8)
+            {
+                var x = extism_input_load_u64(i);
+                var bytes = BitConverter.GetBytes(x);
+                Array.Copy(bytes, 0, buffer, i, bytes.Length);
+                i += 7;
+            }
+            else
+            {
+                buffer[i] = extism_input_load_u8(i);
+            }
+        }
+
+        return buffer;
     }
 }
