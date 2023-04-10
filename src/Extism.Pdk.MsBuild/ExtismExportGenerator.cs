5faccbbcb5e325717bbfe136b2345e2db0f5b9f9
Muhammad Azeez <muhammad-azeez@outlook.com>
2023-04-11 01:43:52 +0300

    feat: the custom task now works :D

--- src/Extism.Pdk.MsBuild/ExtismExportGenerator.cs
+++ src/Extism.Pdk.MsBuild/ExtismExportGenerator.cs
@@ -28,12 +28,12 @@ namespace Extism.Pdk.MsBuild
 
         private void GenerateGlueCode()
         {
+            var assemblyFileName = Path.GetFileName(AssemblyPath);
             var assembly = Mono.Cecil.AssemblyDefinition.ReadAssembly(AssemblyPath);
             var exportedMethods = assembly.MainModule.Types
                 .SelectMany(t => t.Methods)
-                .Where(m => m.IsStatic && m.CustomAttributes.Any(a => a.AttributeType.Name == "ExtismExportAttribute"));
-
-            var assemblyName = Path.GetFileNameWithoutExtension(assembly.FullName);
+                .Where(m => m.IsStatic && m.CustomAttributes.Any(a => a.AttributeType.Name == "ExtismExportAttribute"))
+                .ToArray();
 
             var sb = new StringBuilder();
             sb.AppendLine("#pragma once");
@@ -45,6 +45,8 @@ namespace Extism.Pdk.MsBuild
 
             foreach (var method in exportedMethods)
             {
+                var attribute = method.CustomAttributes.First(a => a.AttributeType.Name == "ExtismExportAttribute");
+                var functionName = attribute.ConstructorArguments[0].Value.ToString();
                 var methodName = method.Name;
                 var parameterCount = method.Parameters.Count;
                 var methodParams = string.Join(", ", Enumerable.Repeat("NULL", parameterCount));
@@ -52,28 +54,18 @@ namespace Extism.Pdk.MsBuild
 
                 sb.AppendLine();
                 sb.AppendLine($@"
-#pragma once
-#define NDEBUG
-// https://github.com/dotnet/runtime/blob/v7.0.0/src/mono/wasi/mono-wasi-driver/driver.c
-#include <string.h>
-
-#include <mono-wasi/driver.h>
-#include <mono/metadata/exception.h>
-#include <assert.h>
-
-MonoMethod* method;
-__attribute__((export_name(""
-                {assemblyName.ToLower()}_export""))) int {assemblyName.ToLower()}_export()
+MonoMethod* method_{functionName};
+__attribute__((export_name(""{functionName}""))) int {functionName}()
 {{
-    if (!method)
+    if (!method_{functionName})
     {{
-        method = lookup_dotnet_method(""{assemblyName}.dll"", ""{assemblyName}"", ""Functions"", ""{{0}}"", -1);
-        assert(method);
+        method_{functionName} = lookup_dotnet_method(""{assemblyFileName}"", ""{method.DeclaringType.Namespace}"", ""{method.DeclaringType.Name}"", ""{methodName}"", -1);
+        assert(method_{functionName});
     }}
 
     void* method_params[] = {{ }};
     MonoObject* exception;
-    MonoObject* result = mono_wasm_invoke_method(method, NULL, method_params, &exception);
+    MonoObject* result = mono_wasm_invoke_method(method_{functionName}, NULL, method_params, &exception);
     assert(!exception);
 
     int int_result = *(int*)mono_object_unbox(result);
